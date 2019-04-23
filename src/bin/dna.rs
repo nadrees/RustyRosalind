3335c54588a5b0c7b9d514ed588ad6f6a68487e8
Nathen Drees <nadrees@users.noreply.github.com>
2019-04-22 21:10:17 -0500

    Restarted project

--- src/bin/dna.rs
+++ src/bin/dna.rs
@@ -1,27 +1,45 @@
-use std::collections::HashMap;
-use std::io;
-use RustyRosalind::{DNAChain, DNA};
+use clap::{App, Arg};
+use rusty_rosalind::nucleotides::DNA;
+use std::fs;
+
+struct Counts {
+    a: u32,
+    c: u32,
+    g: u32,
+    t: u32,
+}
 
 fn main() {
-  println!("Paste input DNA string:");
-
-  let mut input = String::new();
-  io::stdin().read_line(&mut input).unwrap();
-  input.trim();
-
-  let nucleotides = DNAChain::parse_nucleotides(&input);
-  let mut counts: HashMap<DNA, u32> = HashMap::new();
-
-  for nucleotide in nucleotides {
-    let count = counts.entry(nucleotide).or_insert(0);
-    *count += 1;
-  }
-
-  println!(
-    "{} {} {} {}",
-    counts[&DNA::A],
-    counts[&DNA::C],
-    counts[&DNA::G],
-    counts[&DNA::T]
-  );
+    let matches = App::new("DNA")
+        .arg(Arg::with_name("input_file").required(true).index(1))
+        .get_matches();
+    let f_name = matches.value_of("input_file").unwrap();
+    let dna = DNA::parse_str(fs::read_to_string(f_name).unwrap().as_ref());
+    let result = dna.iter().fold(
+        Counts {
+            a: 0,
+            c: 0,
+            g: 0,
+            t: 0,
+        },
+        |current_counts, nucleotide| match nucleotide {
+            DNA::A => Counts {
+                a: current_counts.a + 1,
+                ..current_counts
+            },
+            DNA::C => Counts {
+                c: current_counts.c + 1,
+                ..current_counts
+            },
+            DNA::G => Counts {
+                g: current_counts.g + 1,
+                ..current_counts
+            },
+            DNA::T => Counts {
+                t: current_counts.t + 1,
+                ..current_counts
+            },
+        },
+    );
+    println!("{} {} {} {}", result.a, result.c, result.g, result.t);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,87 +1,26 @@
-use std::fmt;
+pub mod nucleotides {
+    /// The 4 base nucleotides that make up a DNA strand
+    pub enum DNA { A, C, G, T }
 
-#[derive(PartialEq, Eq, Hash)]
-pub enum DNA {
-  A,
-  C,
-  G,
-  T,
-}
+    impl DNA {
+        /// converts a character to its DNA strand, or panics
+        pub fn parse(c: char) -> DNA {
+            match c {
+                'A' => DNA::A,
+                'C' => DNA::C,
+                'G' => DNA::G,
+                'T' => DNA::T,
+                x => panic!("Invalid DNA nucleotide: {:?}", x)
+            }
+        }
 
-#[derive(PartialEq, Eq, Hash)]
-pub enum RNA {
-  A,
-  C,
-  G,
-  U,
-}
-
-impl From<&DNA> for RNA {
-  fn from(n: &DNA) -> RNA {
-    match n {
-      DNA::A => RNA::A,
-      DNA::C => RNA::C,
-      DNA::G => RNA::G,
-      DNA::T => RNA::U,
+        /// converts a string to a DNA string, or panics
+        /// if any character is invalid.
+        pub fn parse_str(s: &str) -> Vec<DNA> {
+            s.trim()
+                .chars()
+                .map(|c| DNA::parse(c))
+                .collect()
+        }
     }
-  }
-}
-
-impl fmt::Display for RNA {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(
-      f,
-      "{}",
-      match &self {
-        RNA::A => 'A',
-        RNA::C => 'C',
-        RNA::G => 'G',
-        RNA::U => 'U',
-      }
-    )
-  }
-}
-
-pub struct DNAChain(Vec<DNA>);
-
-impl DNAChain {
-  pub fn parse_nucleotides(input: &str) -> DNAChain {
-    DNAChain(
-      input
-        .trim()
-        .chars()
-        .map(|c| match c {
-          'A' => DNA::A,
-          'C' => DNA::C,
-          'G' => DNA::G,
-          'T' => DNA::T,
-          x => panic!("Invalid character read: {}", x),
-        })
-        .collect(),
-    )
-  }
-}
-
-impl IntoIterator for DNAChain {
-  type Item = DNA;
-  type IntoIter = ::std::vec::IntoIter<DNA>;
-
-  fn into_iter(self) -> Self::IntoIter {
-    self.0.into_iter()
-  }
-}
-
-pub struct RNAChain(Vec<RNA>);
-
-impl From<DNAChain> for RNAChain {
-  fn from(other: DNAChain) -> RNAChain {
-    RNAChain(other.0.iter().map(|c| RNA::from(c)).collect())
-  }
-}
-
-impl fmt::Display for RNAChain {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    let formatted_results: Vec<String> = self.0.iter().map(|c| format!("{}", c)).collect();
-    write!(f, "{}", formatted_results.join(""))
-  }
 }
